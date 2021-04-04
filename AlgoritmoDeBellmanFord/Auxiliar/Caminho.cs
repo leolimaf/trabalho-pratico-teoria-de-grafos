4c1cf5efdbc0bf006bdf81ad4992883cf4573081
Leonardo Lima <leolimaf@outlook.com>
2021-04-04 18:59:52 -0300

    Implementação do Algoritmo de Bellman Ford

---
+++ AlgoritmoDeBellmanFord/Auxiliar/Caminho.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgoritmoDeBellmanFord.Auxiliar
+{
+    class Caminho
+    {
+        public string PartindoDe { get; set; }
+        public string IndoPara { get; set; }
+        public int Custo { get; set; }
+
+        public Caminho(string partindoDe, string indoPara, int custo)
+        {
+            PartindoDe = partindoDe;
+            IndoPara = indoPara;
+            Custo = custo;
+        }
+    }
+}
--- AlgoritmoDeBellmanFord/Program.cs
+++ AlgoritmoDeBellmanFord/Program.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using AlgoritmoDeBellmanFord.Auxiliar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgoritmoDeBellmanFord
 {
     class Program
     {
+        static string[] vertices = { "S", "A", "B", "C", "D", "E" };
+
+        static List<Caminho> grafo = new List<Caminho>()
+        {
+            new Caminho(partindoDe: "S", indoPara: "A", custo: 10 ),
+            new Caminho(partindoDe: "S", indoPara: "E", custo: 8 ),
+            new Caminho(partindoDe: "A", indoPara: "C", custo: 2 ),
+            new Caminho(partindoDe: "B", indoPara: "A", custo: 1 ),
+            new Caminho(partindoDe: "C", indoPara: "B", custo: -2 ),
+            new Caminho(partindoDe: "D", indoPara: "C", custo: -1 ),
+            new Caminho(partindoDe: "D", indoPara: "A", custo: -4 ),
+            new Caminho(partindoDe: "E", indoPara: "D", custo: 1 )
+        };
+
+        static Dictionary<string, int> memorizando = new Dictionary<string, int>()
+        {
+            {"S", 0 },
+            {"A", int.MaxValue },
+            {"B", int.MaxValue },
+            {"C", int.MaxValue },
+            {"D", int.MaxValue },
+            {"E", int.MaxValue }
+        };
+
+        static bool Iterar()
+        {
+            bool fazerDenovo = false;
+
+            foreach (var vertice in vertices)
+            {
+                Caminho[] arestas = grafo.Where(x => x.PartindoDe == vertice).ToArray();
+                foreach (var aresta in arestas)
+                {
+                    int custoPotencial = 0;
+                    if (memorizando[aresta.PartindoDe] == int.MaxValue)
+                        custoPotencial = int.MaxValue;
+                    else
+                        custoPotencial = memorizando[aresta.PartindoDe] + aresta.Custo;
+
+                    if (custoPotencial < memorizando[aresta.IndoPara])
+                    {
+                        memorizando[aresta.IndoPara] = custoPotencial;
+                        fazerDenovo = true;
+                    }
+                }
+            }
+
+            return fazerDenovo;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                if (!Iterar())
+                    break;
+            }
+
+            foreach (var chaveValor in memorizando)
+            {
+                Console.WriteLine($"{chaveValor.Key}: {chaveValor.Value}");
+            }
         }
     }
 }
