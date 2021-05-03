4662968e4b48759b5f6b3342a3ae54cf91483873
Leonardo Lima <leolimaf@outlook.com>
2021-05-03 18:35:36 -0300

    Início da implementação do algoritmo de Floyd-Warshall

---
+++ AlgoritmoDeFloydWarshall/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlgoritmoDeFloydWarshall
+{
+    class Program
+    {
+        public static readonly int INF = int.MaxValue;
+        static void Main(string[] args)
+        {
+            int[,] grafo =
+            {
+                { 0,    3,    4,    INF },
+                { INF,  0,    INF,  5 },
+                { INF,  INF,  0,    3 },
+                { 8,    INF,  INF,  0}
+            };
+
+            FloydWarshall(grafo);
+        }
+
+        private static void FloydWarshall(int[,] grafo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
