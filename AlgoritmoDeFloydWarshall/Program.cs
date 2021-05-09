3026079bd8179b6e005be5ac81f030d2643033b3
Leonardo Lima <leolimaf@outlook.com>
2021-05-09 17:44:50 -0300

    Implementação do Algoritmo de Floyd Warshall

--- AlgoritmoDeFloydWarshall/Program.cs
+++ AlgoritmoDeFloydWarshall/Program.cs
@@ -4,7 +4,7 @@ namespace AlgoritmoDeFloydWarshall
 {
     class Program
     {
-        public static readonly int INF = int.MaxValue;
+        public static readonly int INF = 999999;
         static void Main(string[] args)
         {
             int[,] grafo =
@@ -20,7 +20,48 @@ namespace AlgoritmoDeFloydWarshall
 
         private static void FloydWarshall(int[,] grafo)
         {
-            throw new NotImplementedException();
+            int qntVertices = (int)Math.Sqrt(grafo.Length);
+            int[,] distancia = new int[qntVertices, qntVertices];
+
+            for (int i = 0; i < qntVertices; i++)
+            {
+                for (int j = 0; j < qntVertices; j++)
+                {
+                    distancia[i, j] = grafo[i, j];
+                }
+            }
+
+            for (int k = 0; k < qntVertices; k++)
+            {
+                for (int i = 0; i < qntVertices; i++)
+                {
+                    for (int j = 0; j < qntVertices; j++)
+                    {
+                        if (distancia[i, k] + distancia[k, j] < distancia[i, j])
+                        {
+                            distancia[i, j] = distancia[i, k] + distancia[k, j];
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Menor distância entre todos pares de vértices: ");
+
+            for (int i = 0; i < qntVertices; i++)
+            {
+                for (int j = 0; j < qntVertices; j++)
+                {
+                    if (distancia[i, j] == INF)
+                    {
+                        Console.Write("INF".PadLeft(7));
+                    }
+                    else
+                    {
+                        Console.Write(distancia[i, j].ToString().PadLeft(7));
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
